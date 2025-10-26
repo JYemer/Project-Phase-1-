e8f589ddb77576b5c44454393a166e021b44b85e
JYemer <133260935+JYemer@users.noreply.github.com>
2025-10-25 20:44:29 -0600

    Add files via upload

---
+++ TCPClient.py
@@ -0,0 +1,11 @@
+from socket import *
+serverName = '10.0.0.87'
+#IP of my desktop which I'm using as a server
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print ('From Server:', modifiedSentence.decode())
+clientSocket.close()
